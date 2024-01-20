23f1ffbefb5250ef1dfee95951d0756a650622f3
F81nj3ct0r <83727511+F81nj3ct0r@users.noreply.github.com>
2024-01-20 16:58:44 -0500

    Update Auto_Info_Gatherer.sh

--- Auto_Info_Gatherer.sh
+++ Auto_Info_Gatherer.sh
@@ -121,7 +121,7 @@ printf "${CYAN}---------------------------${NC}\n"
 printf "${CYAN}Network Device Information:${NC}\n"
 printf "${CYAN}---------------------------${NC}\n" 
 ifconfig -a
-ip
+ip address
 echo ""
 
 printf "${CYAN}---------------------------${NC}\n" 
